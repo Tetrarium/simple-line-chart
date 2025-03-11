f77043b8d11a6b4db4610173b0877dfdd0c7131d
AlexandrTurov <alexandr.turov@mail.ru>
2025-03-11 18:03:26 +0700

    create calculate sections feature

--- src/utils/scores.ts
+++ src/utils/scores.ts
@@ -1,16 +1,20 @@
-function getMean(arr: number[]) {
-  return arr.reduce((a, b) => a + b, 0) / arr.length;
+export function calculateMean(arr: (number | undefined)[]) {
+  const numbers = arr.filter(num => num !== undefined);
+
+  return numbers.reduce((a, b) => a + b, 0) / numbers.length;
 }
 
-export function standardDeviation(arr: number[]) {
-  const average = getMean(arr);
+export function calculateStandardDeviation(arr: (number | undefined)[]) {
+  const numbers = arr.filter(num => num !== undefined);
 
-  const squaredDifferences = arr.map((value) => (value - average) ** 2);
-  return Math.sqrt(getMean(squaredDifferences));
+  const average = calculateMean(numbers);
+
+  const squaredDifferences = numbers.map((value) => (value - average) ** 2);
+  return Math.sqrt(calculateMean(squaredDifferences));
 }
 
-export function getZScores(arr: number[]) {
-  const average = getMean(arr);
-  const standardDev = standardDeviation(arr);
-  return arr.map((value) => (value - average) / standardDev);
+export function getZScores(arr: (number | undefined)[]) {
+  const average = calculateMean(arr);
+  const standardDev = calculateStandardDeviation(arr);
+  return arr.map((value) => value && (value - average) / standardDev);
 }
